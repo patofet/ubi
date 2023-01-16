383a2fd7311b4304789925c10370230b07ec4ba6
eduardJobchain <eduard@jobchain.com>
2023-01-16 11:48:55 +0100

    Commit last changes Wallet and permissions

--- contracts/dependencies/ERC20Distributor.sol
+++ contracts/dependencies/ERC20Distributor.sol
@@ -13,11 +13,36 @@ abstract contract ERC20Distributor is Context, ERC20, Ownable {
     uint256 public lastTimeReedem;
     uint256 public totalUsers;
 
+    bool private newActiveUsersSet = false;
+
+    address public claimer;
+    address public oracle;
+    address public hotWallet;
+    address public coldWallet;
+
+    uint256 public percentageHotWallet;
+
+    modifier onlyClaimer () {
+        require(claimer == _msgSender(), "Claimer: caller is not the claimer");
+        _;
+    }
+
+    modifier onlyOracle () {
+        require(oracle == _msgSender(), "Oracle: caller is not the oracle");
+        _;
+    }
+
     /**
      * @dev Emitted when distributable tokens are Claimed.
      *
      */
     event Claim(address indexed claimer, uint256 indexed timeReedem, uint256 amount);
+
+    /**
+     * @dev Emitted when active users are updated.
+     *
+     */
+    event ActiveUsers(address indexed oracle, uint256 indexed timeUpdate, uint256 amount);
 
     /** @dev Set the `amount` of active users.
      *
@@ -25,9 +50,12 @@ abstract contract ERC20Distributor is Context, ERC20, Ownable {
      *
      * - `amount` cannot be zero.
      */
-    function setActiveUsers(uint256 amount) internal virtual {
+    function setActiveUsers(uint256 amount) public onlyOracle {
         require(amount > 0, "setActiveUsers: active users accont be 0");
         totalUsers = amount;
+
+        newActiveUsersSet = true;
+        emit ActiveUsers(_msgSender(), block.timestamp, totalUsers);
     }
 
     /** @dev Creates the distributable tokens and assigns them to sender account, increasing
@@ -36,28 +64,39 @@ abstract contract ERC20Distributor is Context, ERC20, Ownable {
      * Emits a {Claim} event with the information of the Claim (claimer, timeRedeem and amountClaimed).
      *
      */
-    function claim() internal virtual {
+    function claim() public onlyClaimer {
         require(block.timestamp - lastTimeReedem >= oneMonthSeconds, "claim: not enough time has passed");
+        require(newActiveUsersSet, "claim: monthly active users not updated");
         
         uint256 amountToMint = totalUsers * amountPerUser;
-        _mint(_msgSender(), amountToMint);
+        uint256 amountHotWallet = amountToMint * percentageHotWallet / 10000;
+
+        _mint(hotWallet, amountHotWallet);
+        _mint(coldWallet, amountToMint - amountHotWallet);
         
         lastTimeReedem = block.timestamp;
         emit Claim(_msgSender(), lastTimeReedem, amountToMint);
     }
 
-    /** @dev Set the `amount` of active users, creates the distributable tokens
-     * and assigns them to sender account, increasing the total supply.
-     *
-     * Emits a {Claim} event with the information of the Claim (claimer, timeRedeem and amountClaimed).
-     *
-     * Requirements:
-     *
-     * - `amount` cannot be zero.
-     * - the caller of the function must be the owner of the Smart Contract.
-     */
-    function setUserAndClaim(uint256 amount) public onlyOwner{
-        setActiveUsers(amount);
-        claim();
+    function setClaimer(address _claimer) public onlyOwner {
+        claimer = _claimer;
     }
+
+    function setOracle(address _oracle) public onlyOwner {
+        oracle = _oracle;
+    }
+
+    function setHotWallet(address _hotWallet) public onlyOwner {
+        hotWallet = _hotWallet;
+    }
+
+    function setColdWallet(address _coldWallet) public onlyOwner {
+        coldWallet = _coldWallet;
+    }
+
+    function setPercentageHotWallet(uint256 _percentageHotWallet) public onlyOwner {
+        require(_percentageHotWallet <= 10000 && _percentageHotWallet >= 0, "setPercentageHotWallet: the percentage should be between 0 and 100");
+
+        percentageHotWallet = _percentageHotWallet;
+    }      
 }
